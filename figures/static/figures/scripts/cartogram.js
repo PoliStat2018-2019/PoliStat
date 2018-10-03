a716adb2cd5da9431cbec99e43e1a14673d4743d
Daniel Zhu <dzguangdai@gmail.com>
2018-10-03 10:33:39 -0400

    Remove logging

--- figures/static/figures/scripts/cartogram.js
+++ figures/static/figures/scripts/cartogram.js
@@ -58,7 +58,6 @@ jQuery(document).ready(
 				return "rgb("+r+","+g+","+b+")";
 			}
 			$.getJSON("/raw", function(json) {
-			    console.log(json);
 			    for (var i = 1; i <= 435; ++i) {
 					document.getElementById("jqvmap1_"+i).setAttributeNS(null, 'fill', color(json[i]));
 				}
