e37947e525081d2182008161b9cf5eaf836337b4
Daniel Zhu <dzguangdai@gmail.com>
2018-10-03 10:32:51 -0400

    Cartogram shading

--- figures/static/figures/scripts/cartogram.js
+++ figures/static/figures/scripts/cartogram.js
@@ -1,3 +1,4 @@
+var color; // TODO remove
 jQuery(document).ready(
 		function() {
 			jQuery('#vmap').vectorMap({
@@ -5,7 +6,7 @@ jQuery(document).ready(
 				enableZoom : false,
 				showTooltip : true,
 				selectedColor : null,
-				hoverColor : '#639',
+				hoverColor: null,
 				backgroundColor : '#ffffff00',
 				borderWidth : 0.05,
 				onRegionClick : function(event, code, region) {
@@ -33,4 +34,33 @@ jQuery(document).ready(
 				var path = createPath(pathdata[k], "outline-" + k);
 				paths.appendChild(path);
 			}
+			
+			color = function(dem_p) {
+				var g;
+				if (dem_p < 0.3) {
+					g = dem_p*700;
+					b = g;
+					r = 255;
+				} else if (dem_p > 0.7) {
+					g = (1-dem_p)*700;
+					r = g;
+					b = 255;
+				} else {
+					g = 210;
+					if (dem_p < 0.5) {
+						r = 255;
+						b = 210 + (255-210)/0.2*(dem_p-0.3);
+					} else {
+						b = 255;
+						r = 210 + (255-210)/0.2*(0.7-dem_p);
+					}
+				}
+				return "rgb("+r+","+g+","+b+")";
+			}
+			$.getJSON("/raw", function(json) {
+			    console.log(json);
+			    for (var i = 1; i <= 435; ++i) {
+					document.getElementById("jqvmap1_"+i).setAttributeNS(null, 'fill', color(json[i]));
+				}
+			});
 		});
