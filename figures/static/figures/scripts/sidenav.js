c0312c44665d2e2c1a71e3b9ebab7cc6d833f1b0
Mark <markojungo@gmail.com>
2018-10-04 17:28:27 -0400

    Fade-in blog posts on scroll using AOS

--- figures/static/figures/scripts/sidenav.js
+++ figures/static/figures/scripts/sidenav.js
@@ -2,15 +2,18 @@ $(function() {
     var mobile_button = document.getElementById('sidenav-mobile-btn');
     var sidenav = document.getElementById('sidenav-mobile-target');
     var main = document.getElementById('main');
+    var footer = document.getElementById('footer');
 
     function showNavOnMobile() {
         sidenav.classList.add("show-nav");
-        main.classList.add("darken-main");
+        main.classList.add("darken-on-navbar");
+        footer.classList.add("darken-on-navbar");
     }
     
     function hideNavOnMobile() {
         sidenav.classList.remove("show-nav");
-        main.classList.remove("darken-main");
+        main.classList.remove("darken-on-navbar");
+        footer.classList.remove("darken-on-navbar");
     }
 
     mobile_button.addEventListener("click", showNavOnMobile);
