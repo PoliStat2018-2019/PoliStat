b3ec93b864101d1d473157a105b9f5d0d3609d85
Daniel Zhu <dzguangdai@gmail.com>
2018-10-10 20:54:51 -0400

    "Fix" problems with >100% vote shares

--- figures/templatetags/percentage.py
+++ figures/templatetags/percentage.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter
 def percentage(value):
     return format(value, ".1%")
+
+@register.filter
+def pretty_percentage(value):
+    if value < 0.05:
+        return "near 0%"
+    elif value > 0.95:
+        return "near 100%"
+    return format(value, ".1%")
